2836241ee3a51037b679dbac7e14552db0de85d7
samueljohn <github@samueljohn.de>
2012-07-14 12:27:34 +0200

    Support Xcode-only build.
    - Patch included to respect CFLAGS/CXXFLAGS and CPPFLAGS in order to allow to build with Xcode only (no Command Line Tools needed)
    - Remove -I entry from LDFLAGS because in deps/Makefile there is a find command that searches all -L pathes in LDFLAGS. The "-I" in LDFLAGS is not substituted.

--- julia.rb
+++ julia.rb
@@ -21,12 +21,19 @@ class Julia < Formula
   # Fixes strip issues, thanks to @nolta
   skip_clean 'bin'
 
+
+  def patches
+    DATA
+  end
+
+
   def install
     ENV.fortran
-    ENV.deparallelize
 
     # Julia ignores CPPFLAGS and only uses CFLAGS, so we must store CPPFLAGS into CFLAGS
     ENV.append_to_cflags ENV['CPPFLAGS']
+
+    ENV.remove 'LDFLAGS', "-I#{MacOS.sdk_path}/usr/include"
 
     # Hack to allow julia to get the git version on demand
     ENV['GIT_DIR'] = cached_download/'.git'
@@ -78,3 +85,99 @@ class Julia < Formula
     system "#{bin}/julia", "runtests.jl", "all"
   end
 end
+
+__END__
+diff --git a/Make.inc b/Make.inc
+index 8a13c2e..8adb50e 100644
+--- a/Make.inc
++++ b/Make.inc
+@@ -61,8 +61,8 @@ DEBUGFLAGS = -g -DDEBUG
+ SHIPFLAGS = -O3 -DNDEBUG
+ ifeq ($(OS), Darwin)
+ # use $(sw_vers -productVersion) instead of 10.5?
+-CC += -mmacosx-version-min=10.5
+-CXX += -mmacosx-version-min=10.5
++CC += $(CFLAGS)
++CXX += $(CXXFLAGS)
+ endif
+ endif
+ 
+diff --git a/base/Makefile b/base/Makefile
+index 8d97532..3e09082 100644
+--- a/base/Makefile
++++ b/base/Makefile
+@@ -9,7 +9,7 @@ pcre_h.jl:
+ 	$(QUIET_PERL) ${CC} -E -dM $(shell $(PCRE_CONFIG) --prefix)/include/pcre.h | perl -nle '/^\s*#define\s+(PCRE\w*)\s*\(?($(PCRE_CONST))\)?\s*$$/ and print "const $$1 = int32($$2)"' | sort > $@
+ 
+ errno_h.jl:
+-	$(QUIET_PERL) echo '#include "errno.h"' | cpp -dM - | perl -nle 'print "const $$1 = int32($$2)" if /^#define\s+(E\w+)\s+(\d+)\s*$$/' | sort > $@
++	$(QUIET_PERL) echo '#include "errno.h"' | cpp -I$(SDKROOT)/usr/include -dM - | perl -nle 'print "const $$1 = int32($$2)" if /^#define\s+(E\w+)\s+(\d+)\s*$$/' | sort > $@
+ 
+ os_detect.jl: ../src/os_detect.h
+ 	$(QUIET_PERL) ${CC} -E -P -DJULIA ../src/os_detect.h | perl -p -e 's/\\n/\n/g' > $@
+diff --git a/deps/Makefile b/deps/Makefile
+index 1cd908e..698378d 100644
+--- a/deps/Makefile
++++ b/deps/Makefile
+@@ -635,7 +635,7 @@ distclean-suitesparse: clean-suitesparse
+ # SUITESPARSE WRAPPER
+ 
+ ifeq ($(USE_SYSTEM_SUITESPARSE), 1)
+-SUITESPARSE_INC = -I /usr/include/suitesparse
++SUITESPARSE_INC = -I $(shell ${HOMEBREW_BREW_FILE} --prefix)/include/suitesparse
+ SUITESPARSE_LIB = -lumfpack -lcholmod -lamd -lcamd -lcolamd
+ else
+ SUITESPARSE_INC = -I SuiteSparse-$(SUITESPARSE_VER)/CHOLMOD/Include -I SuiteSparse-$(SUITESPARSE_VER)/SuiteSparse_config -L$(USRLIB)
+@@ -796,7 +796,7 @@ distclean-gmp-wrapper: clean-gmp-wrapper
+ ## GLPK ##
+ 
+ ifeq ($(USE_SYSTEM_GLPK), 1)
+-GLPK_PREFIX = /usr/
++GLPK_PREFIX = $(shell ${HOMEBREW_BREW_FILE} --prefix)/
+ GLPK_OBJ_TARGET =
+ else
+ GLPK_PREFIX = glpk-$(GLPK_VER)/
+@@ -830,7 +830,7 @@ distclean-glpk: clean-glpk
+ ## GLPK Wrapper
+ 
+ ifeq ($(USE_SYSTEM_GLPK), 1)
+-GLPKW_INC = -I /usr/include/
++GLPKW_INC = -I $(shell ${HOMEBREW_BREW_FILE} --prefix)/include/
+ GLPKW_LIB = -lglpk
+ else
+ GLPKW_INC = -I $(abspath $(USR))/include/
+diff --git a/deps/Rmath/src/Makefile b/deps/Rmath/src/Makefile
+index a6f33dc..f1d1ab2 100644
+--- a/deps/Rmath/src/Makefile
++++ b/deps/Rmath/src/Makefile
+@@ -42,7 +42,7 @@ release debug: libRmath.$(SHLIB_EXT)
+ 
+ libRmath.$(SHLIB_EXT): $(XOBJS)
+ 	rm -rf $@
+-	$(QUIET_LINK) $(CC) -shared -o $@ $^ -L$(USRLIB) -lrandom $(RPATH_ORIGIN)
++	$(QUIET_LINK) $(CC) $(LDFLAGS) -shared -o $@ $^ -L$(USRLIB) -lrandom $(RPATH_ORIGIN)
+ 
+ clean:
+ 	rm -f *.o *.do *.a *.$(SHLIB_EXT) core* *~ *#
+diff --git a/extras/Makefile b/extras/Makefile
+index 0c4a0fd..5aea51d 100644
+--- a/extras/Makefile
++++ b/extras/Makefile
+@@ -8,7 +8,7 @@ GLPK_VER = 4.47
+ GLPK_CONST = 0x[0-9a-fA-F]+|[-+]?\s*[0-9]+
+ 
+ ifeq ($(USE_SYSTEM_GLPK), 1)
+-GLPK_PREFIX = /usr/include
++GLPK_PREFIX = $(shell ${HOMEBREW_BREW_FILE} --prefix)/include
+ else
+ GLPK_PREFIX = $(JULIAHOME)/deps/glpk-$(GLPK_VER)/src
+ endif
+@@ -17,7 +17,7 @@ glpk_h.jl:
+ 	$(QUIET_PERL) cpp -dM $(GLPK_PREFIX)/glpk.h | perl -nle '/^\s*#define\s+(GLP\w*)\s*\(?($(GLPK_CONST))\)?\s*$$/ and print "const $$1 = int32($$2)"' | sort > $@
+ 
+ julia_message_types_h.jl: ../ui/webserver/message_types.h
+-	$(QUIET_PERL) cpp -Dnotdefined $^ > $@
++	$(QUIET_PERL) cpp -I$(SDKROOT)/usr/include -Dnotdefined $^ > $@
+ 
+ clean:
+ 	rm -f glpk_h.jl julia_message_types_h.jl
