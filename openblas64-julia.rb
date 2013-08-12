02815cf85da1b034f6a398d9838c5afb27f6bf71
Elliot Saba <staticfloat@gmail.com>
2013-08-12 13:55:57 -0700

    Update openblas64-julia to download tarball instead of zipball. (Closes #39)

--- openblas64-julia.rb
+++ openblas64-julia.rb
@@ -4,7 +4,7 @@ class Openblas64Julia < Formula
   homepage 'http://xianyi.github.com/OpenBLAS/'
   # Maintainers, remember to update the LAPACK url in OpenblasLapack above.
   # See the "LAPACK_URL" in the openblas Makefile for the right version.
-  url 'http://github.com/xianyi/OpenBLAS/zipball/v0.2.8'
+  url 'http://github.com/xianyi/OpenBLAS/archive/v0.2.8.tar.gz'
   sha1 'd012ebc2b8dcd3e95f667dff08318a81479a47c3'
   head "https://github.com/xianyi/OpenBLAS.git", :branch => "develop"
 
