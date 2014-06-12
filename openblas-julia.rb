883798bb06d302954b7b549f1116299f1fd954e7
Elliot Saba <staticfloat@gmail.com>
2014-06-12 11:04:59 -0700

    Update to OpenBLAS 0.2.9

--- openblas-julia.rb
+++ openblas-julia.rb
@@ -4,8 +4,8 @@ class OpenblasJulia < Formula
   homepage 'http://xianyi.github.com/OpenBLAS/'
   # Maintainers, remember to update the LAPACK url in OpenblasLapack above.
   # See the "LAPACK_URL" in the openblas Makefile for the right version.
-  url 'http://github.com/xianyi/OpenBLAS/archive/v0.2.8.tar.gz'
-  sha1 'd012ebc2b8dcd3e95f667dff08318a81479a47c3'
+  url 'http://github.com/xianyi/OpenBLAS/archive/v0.2.9.tar.gz'
+  sha1 'e8197711c4fe43ac286366693bd7c1683003c894'
   head "https://github.com/xianyi/OpenBLAS.git", :branch => "develop"
 
   depends_on :fortran
