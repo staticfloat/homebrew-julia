bd944496bd6b32d111b14928a3610f20912e1a9c
Elliot Saba <staticfloat@gmail.com>
2014-07-16 21:44:36 -0400

    Fix a ruby typo

--- openblas-julia.rb
+++ openblas-julia.rb
@@ -4,8 +4,8 @@ class OpenblasJulia < Formula
   homepage 'http://xianyi.github.com/OpenBLAS/'
   # Maintainers, remember to update the LAPACK url in OpenblasLapack above.
   # See the "LAPACK_URL" in the openblas Makefile for the right version.
-  url 'http://github.com/xianyi/OpenBLAS/archive/v0.2.9.tar.gz'
-  sha1 'e8197711c4fe43ac286366693bd7c1683003c894'
+  url 'http://github.com/xianyi/OpenBLAS/archive/v0.2.10.tar.gz'
+  sha1 'bcc55e335792874b00a9f3460e7e9778ded3b848'
   head "https://github.com/xianyi/OpenBLAS.git", :branch => "develop"
 
   depends_on :fortran
@@ -24,9 +24,9 @@ class OpenblasJulia < Formula
     end
     system "make", "PREFIX=#{prefix}", "install"
     cd "#{lib}" do
-      dylib = Dir.glob("libopenblas_*.dylib")
+      dylib = Dir.glob("libopenblas_*.dylib")[0]
       system "ln", "-sf", dylib, "libopenblas.dylib"
-      system "strip", "-S", dylib
+      system "strip", "-S", "libopenblas.dylib"
     end
   end
 end
