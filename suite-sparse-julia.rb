2528cd0e56aee9f9a95d03bf3e97128bc00ab0a2
Elliot Saba <staticfloat@gmail.com>
2014-07-23 16:39:36 -0400

    Bottle suite-sparse-julia

--- suite-sparse-julia.rb
+++ suite-sparse-julia.rb
@@ -5,14 +5,20 @@ class SuiteSparseJulia < Formula
   mirror 'http://d304tytmzqn1fl.cloudfront.net/SuiteSparse-4.2.1.tar.gz'
   sha1 '2fec3bf93314bd14cbb7470c0a2c294988096ed6'
 
+  bottle do
+    root_url 'https://juliabottles.s3.amazonaws.com'
+    cellar :any
+    sha1 'f7111df67b8162ee10ca4cc84431153304dfd8f3' => :lion
+    sha1 '44a9726dae5bc711a9734d5e8b23c9301d86ce05' => :mavericks
+    sha1 '16db0dd7fc69a535e7bed985db90be29c2febd29' => :mountain_lion
+  end
+
   depends_on "tbb" => :optional
   depends_on "metis" => :optional
   depends_on "staticfloat/julia/openblas-julia" if build.without? 'accelerate'
 
   option "with-metis", "Compile in metis libraries"
   option 'with-accelerate', 'Compile against Accelerate/vecLib instead of OpenBLAS'
-
-  keg_only "Conflicts with suite-sparse"
 
   def install
     # SuiteSparse doesn't like to build in parallel
