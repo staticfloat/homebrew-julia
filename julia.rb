55c125aff94e052861a7fb675aa62b96acfcdf54
Elliot Saba <staticfloat@gmail.com>
2014-09-22 23:00:37 -0700

    Update stable Julia to v0.3.1

--- julia.rb
+++ julia.rb
@@ -26,8 +26,8 @@ class Julia < Formula
   homepage 'http://julialang.org'
 
   stable do
-    url 'https://github.com/JuliaLang/julia.git', :using => GitNoDepthDownloadStrategy, :tag => 'v0.3.0'
-    version '0.3.0'
+    url 'https://github.com/JuliaLang/julia.git', :using => GitNoDepthDownloadStrategy, :tag => 'v0.3.1'
+    version '0.3.1'
   end
 
   head do
