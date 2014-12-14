6c35b61f413de505b56e3547bfed6729b7f68378
Elliot Saba <staticfloat@gmail.com>
2014-12-14 03:12:33 -0800

    Get bottles ready for new GCC stuffage!

--- arpack-julia.rb
+++ arpack-julia.rb
@@ -5,14 +5,14 @@ class ArpackJulia < Formula
   url 'http://forge.scilab.org/index.php/p/arpack-ng/downloads/get/arpack-ng-3.1.3.tar.gz'
   mirror 'http://d304tytmzqn1fl.cloudfront.net/arpack-ng-3.1.3.tar.gz'
   sha1 'c1ac96663916a4e11618e9557636ba1bd1a7b556'
+  revision 1
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    revision 7
-    sha1 "adc58011dc2da865af3b1a864e5964dad13569b6" => :mountain_lion
-    sha1 "9310453422de61f9e8cc1f5bb4964fd3ba89e141" => :mavericks
-    sha1 "c4f9535d652c8166b5a38a961f835d9cf675fa79" => :yosemite
+    sha1 "c423ce133e147e996fd9678b04a6eb299458e63c" => :mountain_lion
+    sha1 "ee130ea5d34ac9a67be239d156a1a7cf082115f4" => :mavericks
+    sha1 "4b8b45f3ff6d9437491f7bc23830e433eb759f63" => :yosemite
   end
   keg_only 'Conflicts with arpack in homebrew-science.'
 
