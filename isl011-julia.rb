2c1fa842098d86db581c0a9c66de98bbed55531e
Elliot Saba <staticfloat@gmail.com>
2015-11-02 23:55:35 -0800

    Bottle isl 11.2

--- isl011-julia.rb
+++ isl011-julia.rb
@@ -9,6 +9,9 @@ class Isl011Julia < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
+    sha256 "2e2b07b71313947f60f20711962b0eb7c3385d443b253138137aa6a0b7f7bdfb" => :mavericks
+    sha256 "bf0d4c6fdd3797dcb543d2cb42061c8dd2658402b8a623b5018737a86e5522b4" => :yosemite
+    sha256 "e9eee9fe806f38bee7648b59d73610500fe27cac6625b4ad7396c2eca5781705" => :el_capitan
   end
 
   keg_only 'Conflicts with isl in main repository.'
