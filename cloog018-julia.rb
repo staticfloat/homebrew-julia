2d01376c76a1dac2e87577129071fdfb188385cc
Elliot Saba <staticfloat@gmail.com>
2015-11-02 23:56:02 -0800

    Prepare cloog for bottling

--- cloog018-julia.rb
+++ cloog018-julia.rb
@@ -3,18 +3,12 @@ require 'formula'
 class Cloog018Julia < Formula
   homepage 'http://www.cloog.org/'
   # Track gcc infrastructure releases.
-  url 'http://www.bastoul.net/cloog/pages/download/count.php3?url=./cloog-0.18.0.tar.gz'
-  mirror 'ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.0.tar.gz'
-  sha1 '85f620a26aabf6a934c44ca40a9799af0952f863'
+  url 'http://www.bastoul.net/cloog/pages/download/count.php3?url=./cloog-0.18.4.tar.gz'
+  mirror 'ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.4.tar.gz'
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    revision 1
-    sha1 'ef93a3f89ec912781315fa206e2731dc2d65bae6' => :lion
-    sha1 'e2ee2363aa35d3e02e47b0f81a727dde031fac32' => :mavericks
-    sha1 '243b762581c65cb9414f9e7ac7d284d78301bcc2' => :mountain_lion
-    sha1 "fab85e4b7244f9ed7c92b0cbbde53cd75c99bfd0" => :yosemite
   end
 
   keg_only 'Conflicts with cloog in main repository.'
