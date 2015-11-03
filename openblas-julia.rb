222ccd6d4ddc65777b38a08f4468648d47c421a8
Elliot Saba <staticfloat@gmail.com>
2015-11-02 20:24:43 -0800

    Prepare openblas-julia for v0.2.15 release

--- openblas-julia.rb
+++ openblas-julia.rb
@@ -2,18 +2,12 @@ require 'formula'
 
 class OpenblasJulia < Formula
   homepage 'http://xianyi.github.com/OpenBLAS/'
-  url 'http://github.com/xianyi/OpenBLAS/archive/v0.2.12.tar.gz'
+  url 'http://github.com/xianyi/OpenBLAS/archive/v0.2.15.tar.gz'
   head "https://github.com/xianyi/OpenBLAS.git", :branch => "develop"
-  sha1 '2bdedca65e29186d1ecaaed45cb6c9b1f3f1c868'
-  revision 1
 
   bottle do
-    revision 2
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha256 "0078f9e4263b31ee90a9397890eddee3bbd25e438fb142abc1fb646a6ae81ef2" => :mavericks
-    sha256 "e54b900019bce9fc095af9b1c124191dd0e2165220a2db5c53256eb75617b833" => :yosemite
-    sha256 "735852acea8b6e2635c7860cd06734c595087323ac3f6b26488ffe0f07527553" => :mountain_lion
   end
 
   depends_on :fortran
