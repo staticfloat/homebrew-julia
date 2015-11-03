d13711e39e9523273bf46d476990108fad947d14
Elliot Saba <staticfloat@gmail.com>
2015-11-02 22:10:53 -0800

    Prepare suite-sparse-julia for bottling

--- suite-sparse-julia.rb
+++ suite-sparse-julia.rb
@@ -1,17 +1,11 @@
 require 'formula'
 class SuiteSparseJulia < Formula
   homepage 'http://www.cise.ufl.edu/research/sparse/SuiteSparse'
-  url 'http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.4.3.tar.gz'
-  mirror 'http://d304tytmzqn1fl.cloudfront.net/SuiteSparse-4.4.3.tar.gz'
-  sha1 '3a583ca4c09c6e9d7c574d313ad93e514478bb29'
+  url 'http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.4.5.tar.gz'
 
   bottle do
-    revision 1
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha256 "f3672a7af2550d137c12945bdfe7c804e65c8d76eae15a0bc5e27520dc283adf" => :mountain_lion
-    sha256 "08151e96b5dbe02b9cac21500f2d17e28670b6ed081f8e46d831c90208422a54" => :yosemite
-    sha256 "998b61c8d0432aa973af5181f96159db98288f5f0e65b787000b299a9b585468" => :mavericks
   end
   keg_only 'Conflicts with suite-sparse in homebrew-science.'
 
