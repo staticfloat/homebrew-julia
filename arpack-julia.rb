f98d9563935b400fc13c37de942d9f7842a0f774
Elliot Saba <staticfloat@gmail.com>
2016-04-02 12:35:53 -0700

    Prepare arpack for rebottling due to openblas version bump

--- arpack-julia.rb
+++ arpack-julia.rb
@@ -1,16 +1,14 @@
 require 'formula'
 
 class ArpackJulia < Formula
-  homepage 'http://forge.scilab.org/index.php/p/arpack-ng'
+  homepage 'https://github.com/opencollab/arpack-ng'
   url 'https://github.com/opencollab/arpack-ng/archive/3.3.0.tar.gz'
-  sha256 'ad59811e7d79d50b8ba19fd908f92a3683d883597b2c7759fdcc38f6311fe5b3'
+  sha256 'ad59811e7d79d50b8ba19fd908f92a3683d883597b2c7759fdcc38f6311fe5b3'i
+  revision 1
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha256 "9cc8bf41f5fe7456274ba57e8a6a184afe4ebe46091ebd03edc22d1234e9853d" => :mavericks
-    sha256 "a51950df67535a2d1ec77fc8c031fde80dbbc5a042a117cabb030fea19e16faf" => :yosemite
-    sha256 "f5e00c2216c43ae509920f46947f04136bcfd003bfa7d666ab0ade6a3feb67df" => :el_capitan
   end
   keg_only 'Conflicts with arpack in homebrew-science.'
 
