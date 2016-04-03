139250a013b1bc741c389d610a8af15aebda0910
Elliot Saba <staticfloat@gmail.com>
2016-04-03 15:41:54 -0700

    Prepare julia v0.4.5 for bottling

--- julia.rb
+++ julia.rb
@@ -16,8 +16,8 @@ class Julia < Formula
 
   stable do
     url 'https://github.com/JuliaLang/julia.git',
-      :using => GitNoDepthDownloadStrategy, :shallow => false, :tag => "v0.4.3"
-    version "0.4.3"
+      :using => GitNoDepthDownloadStrategy, :shallow => false, :tag => "v0.4.5"
+    version "0.4.5"
   end
 
   head do
@@ -28,9 +28,6 @@ class Julia < Formula
   # Remember to clear "revision" above when prepping for new bottles, if it exists
   bottle do
     root_url "https://juliabottles.s3.amazonaws.com"
-    sha256 "7d050b798a5285fd9295086d4e2e1b911909c1f6bbdfc5faa90b6217a1769332" => :yosemite
-    sha256 "492e7cfbad699a6212c6b1552aa1a775e0d1ddeb6f7bad141bf2b1f523e36f0e" => :mavericks
-    sha256 "9da535e4dece98f94576adf95a235829fe446f8ffd8ed9161d174bbe30540829" => :el_capitan
   end
 
   if build.head?
