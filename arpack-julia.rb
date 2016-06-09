4406d2feadd71f2756a617d5d27b5cdc0c8146fc
Elliot Saba <staticfloat@gmail.com>
2016-06-09 10:37:14 -0700

    Prepare arpack and suite-sparse for bottling.  Again.

--- arpack-julia.rb
+++ arpack-julia.rb
@@ -4,15 +4,11 @@ class ArpackJulia < Formula
   homepage 'https://github.com/opencollab/arpack-ng'
   url 'https://github.com/opencollab/arpack-ng/archive/3.3.0.tar.gz'
   sha256 'ad59811e7d79d50b8ba19fd908f92a3683d883597b2c7759fdcc38f6311fe5b3'
-  revision 1
+  revision 2
 
   bottle do
-    revision 1
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha256 "ae09972ab2ee7166e7e42ccbe938185841f78387485d2b9a638c58330785566d" => :el_capitan
-    sha256 "fd160c9c959dc8eef03d72e859e0533b8e2019a3efe68f9d0490e42e046bdccf" => :yosemite
-    sha256 "cd4a275e37881107e12681bd70947c6efa3b92555e03b6cb1c52bc7ece0139ad" => :mavericks
   end
   keg_only 'Conflicts with arpack in homebrew-science.'
 
--- suite-sparse-julia.rb
+++ suite-sparse-julia.rb
@@ -3,15 +3,12 @@ class SuiteSparseJulia < Formula
   homepage 'http://faculty.cse.tamu.edu/davis/suitesparse.html'
   url 'http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.4.6.tar.gz'
   sha256 '7f22509d87ada8506580d537efde79cf90e28e228355c18b8bf603aad1a2d7b6'
+  revision 1
 
   bottle do
-    revision 1
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any_skip_relocation
-    sha256 "b5d1c6cc44aa84ee3afdd2f53e0417a7822794e152a6a83b85616bbc7382aff4" => :mavericks
-    sha256 "40915d5410aa86defd31dc37da9052a7a63ce1cebfafdcde3efbea01fbf1c1f0" => :yosemite
-    sha256 "25445feb003d31eb437cafe5941f8e483f2ca4587d62647115ec62c4cffed608" => :el_capitan
-  end
+  end 
 
   keg_only 'Conflicts with suite-sparse in homebrew-science.'
 
