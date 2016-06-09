8224a4841d576af204ac274587be79d53d200514
Elliot Saba <staticfloat@gmail.com>
2016-06-09 10:52:54 -0700

    Bottle suite-sparse-julia

--- suite-sparse-julia.rb
+++ suite-sparse-julia.rb
@@ -8,6 +8,9 @@ class SuiteSparseJulia < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any_skip_relocation
+    sha256 "8ccda16edde2c8f67361c61404e39d08553feded0ae5a262525a19c0ba733f75" => :mavericks
+    sha256 "2adb96fa54c46cc9098c2a2a3c147be4495e9267caecaaf5358be2ba67e5075b" => :yosemite
+    sha256 "c13e552af6350f0ac838939342e9cb1872745df18c0199e2394e727131d4b9a5" => :el_capitan
   end 
 
   keg_only 'Conflicts with suite-sparse in homebrew-science.'
