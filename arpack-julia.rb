522f98a704fc0cf15f81254e4985f027123d65c7
Elliot Saba <staticfloat@gmail.com>
2016-12-04 16:15:48 -0800

    Bottle arpack-julia

--- arpack-julia.rb
+++ arpack-julia.rb
@@ -6,6 +6,16 @@ class ArpackJulia < Formula
   sha256 'ad59811e7d79d50b8ba19fd908f92a3683d883597b2c7759fdcc38f6311fe5b3'
   revision 4
 
+  bottle do
+    root_url 'https://juliabottles.s3.amazonaws.com'
+    cellar :any
+    rebuild 1
+    sha256 "88474d2569fec1e8e7fe1a3c996441d7c465b75873881c7db3a6168cc91a2922" => :mavericks
+    sha256 "eb58a635b7792754a20d2ebaa00bcaca8d4764fd9eab6e7c92ccab3fec792678" => :yosemite
+    sha256 "8dd57cd970b6bcda1dfafeebc776d863abc7670702327117bb3909212842647f" => :el_capitan
+    sha256 "9270aa1218081787f845de5da6b5e9124f88580c7b09cbed7bb52de4411954a6" => :sierra
+  end
+
   keg_only 'Conflicts with arpack in homebrew-science.'
 
   depends_on :fortran
