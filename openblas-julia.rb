134df22c5a74567b8c06a5b6676146b6019a151f
Elliot Saba <staticfloat@gmail.com>
2017-10-20 23:46:51 -0700

    Prepare openblas-julia `v0.2.20` for bottling

--- openblas-julia.rb
+++ openblas-julia.rb
@@ -2,18 +2,13 @@ require 'formula'
 
 class OpenblasJulia < Formula
   homepage 'http://xianyi.github.com/OpenBLAS/'
-  url 'http://github.com/xianyi/OpenBLAS/archive/v0.2.19.tar.gz'
+  url 'http://github.com/xianyi/OpenBLAS/archive/v0.2.20.tar.gz'
   head 'https://github.com/xianyi/OpenBLAS.git', :branch => 'develop'
-  sha256 '9c40b5e4970f27c5f6911cb0a28aa26b6c83f17418b69f8e5a116bb983ca8557'
+  sha256 '5ef38b15d9c652985774869efd548b8e3e972e1e99475c673b25537ed7bcf394'
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    rebuild 1
-    sha256 "8660959f1b5c4907b26ad719f22a6946983e6845109a056c5c6cc15d4e96785e" => :mavericks
-    sha256 "eac5cd4f0bd509f58faca8006910ab0daa302cafcc72813a306b634d23f52539" => :el_capitan
-    sha256 "607948b2c706b9e4d4926f2a5a90b69bf25edbe8078b26905d12730a87a81bce" => :yosemite
-    sha256 "3c0bd462ac422352318462bcb679f4d465a399f62815fecad5f152a8648fe99c" => :sierra
   end
 
   depends_on :fortran
