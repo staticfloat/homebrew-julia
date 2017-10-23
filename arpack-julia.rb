b036eb173bbb6b9f4b58e3b954c9f12527755217
Elliot Saba <staticfloat@gmail.com>
2017-10-22 17:33:30 -0700

    Rebuild `arpack` for OpenBLAS v0.2.20

--- arpack-julia.rb
+++ arpack-julia.rb
@@ -9,11 +9,9 @@ class ArpackJulia < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    rebuild 1
-    sha256 "88474d2569fec1e8e7fe1a3c996441d7c465b75873881c7db3a6168cc91a2922" => :mavericks
-    sha256 "eb58a635b7792754a20d2ebaa00bcaca8d4764fd9eab6e7c92ccab3fec792678" => :yosemite
-    sha256 "8dd57cd970b6bcda1dfafeebc776d863abc7670702327117bb3909212842647f" => :el_capitan
-    sha256 "9270aa1218081787f845de5da6b5e9124f88580c7b09cbed7bb52de4411954a6" => :sierra
+    rebuild 2
+    sha256 "176bef36b77bf8cf6a92bc5c20f25982783286e2062fd50b7329e43aaa602cf9" => :el_capitan
+    sha256 "535d1aff9d8acb49469bb95de55feb02f68eae3e2f76ee732f5db9f1dba54cc7" => :sierra
   end
 
   keg_only 'Conflicts with arpack in homebrew-science.'
